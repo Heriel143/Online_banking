6252d3f7f2db1d37a3c3fb657421f80060ce6508
Heriel David <herieldavid143@gmail.com>
2022-10-01 10:12:20 +0300

    space removed

--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -2,53 +2,7 @@
 
 @section('admin')
 
-<!-- Trash part -->
 
-{{-- 
-<div class="container">
-    <div class="row">
-     
-    
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">Trash List</div>
-        </div>
-        <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">SL No.</th>
-                <th scope="col">Account Name</th>
-                <th scope="col">Number</th>
-                <th scope="col">User</th>
-                <th scope="col">Balance</th>
-                <th scope="col">Deleted  at</th>
-                <th scope="col">Action</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                @php($i = 1)
-                @foreach($trashAccounts as $account)
-                <tr>
-                <th scope="row">{{ $i++ }}</th>
-                <td>{{ $account->account_name }}</td>
-                <td>{{ $account->account_no }}</td>
-                <td>{{ $account->user->name }}</td>
-                <td>{{ $account->balance }}</td>
-                <td>{{ $account->deleted_at->diffForHumans() }} </td>
-                <td> 
-                    <a href="{{ url('restore/account/'.$account->id) }}" class="btn btn-success">Restore</a> 
-                    <a href="{{ url('pdelete/account/'.$account->id) }}" class="btn btn-danger">Permanent Delete</a> 
-                </td>
-                </tr>
-                @endforeach
-            </tbody>
-
-            {{ $trashAccounts->links() }}
-        </table>
-    </div>
-    </div>
-</div> --}}
 <section class="content">
     <div class="container-fluid">
       <div class="row">
@@ -82,7 +36,7 @@
             <td>{{ $account->account_no }}</td>
             <td>{{ $account->user->name }}</td>
             <td>{{ $account->balance }}</td>
-            <td><p class="{{ $account->status > 0 ? 'bg-success': 'bg-warning' }} border border-radius-8">{{ $account->status > 0 ? 'active' : 'inactive'}}</p></td>
+            <td><span class="badge text-bg-{{ $account->status > 0 ? 'success': 'warning' }} text-center">{{ $account->status > 0 ? 'active' : 'inactive'}}</span></td>
             <td>{{ $account->created_at->diffForHumans() }} </td>
             <td> 
                 <a href="{{ url('account/edit/'.$account->id) }}" class="btn btn-info">Edit</a> 
